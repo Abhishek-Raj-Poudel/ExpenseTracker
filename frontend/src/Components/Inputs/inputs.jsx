f66b02afac73942639176efe61aaf709bad2b76c
Abhishek Raj Poudel <89299685+Abhishek-Raj-Poudel@users.noreply.github.com>
2022-04-08 14:42:58 +0545

    Removes unwanted some code

--- frontend/src/Components/Inputs/inputs.jsx
+++ frontend/src/Components/Inputs/inputs.jsx
@@ -13,18 +13,4 @@ export function Input({ name, label, required }) {
     </>
   );
 }
-export function Select({ label, name, parentId, options, required }) {
-  return (
-    <>
-      <label>{label}</label>{" "}
-      <select name={name} id={name} required={required}>
-        <option value="">--Select Any One--</option>
-        {options.map((obj, index) => (
-          <option key={index} value={obj._id ? obj._id : obj}>
-            {obj.name ? obj.name : obj}
-          </option>
-        ))}
-      </select>
-    </>
-  );
-}
+
