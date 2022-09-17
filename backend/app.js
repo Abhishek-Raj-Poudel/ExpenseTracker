c24119ba4d11baa0543429e68ce9a264246d5e91
unknown <abhishekrajpoudel@gmail.com>
2022-09-17 15:58:59 +0545

    setup mongodb cluster

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.json({
-    data: null,
+    data: req,
     status: err.status,
     message: err,
   });
--- backend/db_init.js
+++ backend/db_init.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const dbUrl = "mongodb://0.0.0.0:27017/office-management-system";
+const dbUrl =
+  "mongodb+srv://chronoabi:22chronoabi31@mern-database-chronoabi.rfhg6de.mongodb.net/office-management-system?retryWrites=true&w=majority";
+//mongodb+srv://chronoabi:<password>@mern-database-chronoabi.rfhg6de.mongodb.net/?retryWrites=true&w=majority
 
 mongoose.connect(dbUrl, (err, success) => {
   if (err) {
